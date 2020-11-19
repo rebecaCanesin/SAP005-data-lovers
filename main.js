f15f89bf1319793b0068a630c98a30e219131c84
g.larangeira <glarangeirag@gmail.com>
2020-11-19 14:07:12 -0300

    primeiro branch

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data);
+
+const dados = data.results;
+
+console.log(dados);
+
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+        document.getElementById('menu').style.borderBottomRightRadius = '0';
+        document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    } else {
+        document.getElementById('menu').style.borderRadius = '41px';
+    }
+}
+
+/*for (const nome in dados) {
+    console.log(dados[nome].name);
+}
+
+for (const vida in dados) {
+    console.log(dados[vida].status);
+}
+
+for (const especie in dados) {
+    console.log(dados[especie].species);
+}
+
+for (const genero in dados) {
+    console.log(dados[genero].gender);
+}
+
+for (const nome in dados) {
+    console.log(dados[nome].name);
+}
+*/
